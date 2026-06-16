4a499538d2e794c103a4f20dd40ec21fd4a62ecc
sahadathussain12 <sahadathussain872856@gmail.com>
2026-06-16 23:47:45 +0600

    feat: add timeline analytics with pie chart

--- src/app/stats/page.jsx
+++ src/app/stats/page.jsx
@@ -1,46 +1,53 @@
-"use client"
-import { FriendsContext } from '@/contex/ContexProvider';
-import React, { useContext } from 'react';
-import { Legend, Pie, PieChart, Tooltip } from 'recharts';
+"use client";
 
- 
-
+import { FriendsContext } from "@/contex/ContexProvider";
+import React, { useContext } from "react";
+import { Legend, Pie, PieChart, Tooltip } from "recharts";
 
 const StatsPage = () => {
-    const { timeline, setTimeline } = useContext(FriendsContext);
+  const { timeline } = useContext(FriendsContext);
 
-    const data = [
-  { name: 'text', value:timeline.length, fill: '#0088FE' },
-  { name: 'audeo', value: timeline.length, fill: '#00C49F' },
-  { name: 'video', value: timeline.length, fill: '#FFBB28' },
-  
-];
+  console.log(timeline,'timeline');
 
-    return (
-        <div className='max-w-5xl mx-auto'>
-          <h1 className='text-3xl font-bold mt-4'>Friendship Analytics</h1>
-  <div className='flex justify-center items-center border border-white shadow-xl m-4 bg-[#FFFFFF]'>
-    
-          <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }} responsive>
-      <Pie
-        data={data}
-        innerRadius="80%"
-        outerRadius="100%"
-        // Corner radius is the rounded edge of each pie slice
-        cornerRadius="50%"
-        fill="#8884d8"
-        // padding angle is the gap between each pie slice
-        paddingAngle={5}
-        dataKey="value"
-        isAnimationActive={true}
-      />
-     <Legend/>
-     <Tooltip/>
-    </PieChart>
+  const textCount = timeline.filter(
+    (item) => item.type?.toLowerCase() === "text"
+  ).length;
 
-  </div>
-        </div>
-    );
+  const audioCount = timeline.filter(
+    (item) => item.type?.toLowerCase() === "call"
+  ).length;
+
+  const videoCount = timeline.filter(
+    (item) => item.type?.toLowerCase() === "video"
+  ).length;
+
+  const data = [
+    { name: "Text", value: textCount, fill: "#0088FE" },
+    { name: "call", value: audioCount, fill: "#00C49F" },
+    { name: "Video", value: videoCount, fill: "#FFBB28" },
+  ];
+
+  return (
+    <div className="max-w-5xl mx-auto">
+      <h1 className="text-3xl font-bold mt-4">Friendship Analytics</h1>
+
+      <div className="flex justify-center items-center border border-white shadow-xl m-4 bg-[#FFFFFF]">
+        <PieChart width={500} height={500}>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            innerRadius={100}
+            outerRadius={150}
+            paddingAngle={5}
+            dataKey="value"
+          />
+          <Tooltip />
+          <Legend />
+        </PieChart>
+      </div>
+    </div>
+  );
 };
 
 export default StatsPage;
